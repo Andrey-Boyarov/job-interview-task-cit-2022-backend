9abc46ba964ac961631dc8a5e8a0b0567fbb957c
boyarov.a <boyarov.a@it-universe.ru>
2022-07-24 06:50:11 +0400

    Initial commit

---
+++ src/main/resources/data-hsqldb.sql
@@ -0,0 +1,4 @@
+insert into dict_task
+values (1, 'Substrings search', 0, 0);
+insert into dict_task
+values (2, 'Magic square', 1, 1);
---
+++ src/main/resources/schema-hsqldb.sql
@@ -0,0 +1,7 @@
+CREATE TABLE dict_task
+(
+    id bigint primary key,
+    label varchar (30),
+    code bigint,
+    input_type bigint
+);
