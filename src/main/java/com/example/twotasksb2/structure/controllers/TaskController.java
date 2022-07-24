5c034f36a484e3ba1e2ea5f32ebeb6d6dc797872
boyarov.a <boyarov.a@it-universe.ru>
2022-07-24 17:54:42 +0400

    input types

--- src/main/java/com/example/twotasksb2/structure/controllers/TaskController.java
+++ src/main/java/com/example/twotasksb2/structure/controllers/TaskController.java
@@ -3,7 +3,10 @@ package com.example.twotasksb2.structure.controllers;
 import com.example.twotasksb2.structure.services.TaskService;
 import com.example.twotasksb2.utils.Option;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +21,10 @@ public class TaskController {
     @GetMapping("/getTasks")
     public List<Option> getTasks(){
         return taskService.getTasks();
+    }
+
+    @GetMapping("/getInputType/{id}")
+    public ResponseEntity<Long> getInputType(@PathVariable Long id){
+        return taskService.getInputType(id);
     }
 }
--- src/main/java/com/example/twotasksb2/structure/services/TaskService.java
+++ src/main/java/com/example/twotasksb2/structure/services/TaskService.java
@@ -4,9 +4,12 @@ import com.example.twotasksb2.structure.entities.DictTask;
 import com.example.twotasksb2.structure.repositories.DictTaskRepository;
 import com.example.twotasksb2.utils.Option;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,5 +19,14 @@ public class TaskService {
 
     public List<Option> getTasks(){
         return dictTaskRepository.findAll().stream().map(t -> new Option(t.getCode(), t.getLabel())).collect(Collectors.toList());
+    }
+
+    public ResponseEntity<Long> getInputType(Long id){
+        Long res = getInputTypeById(id);
+        return res == null ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    private Long getInputTypeById(Long id){
+        return dictTaskRepository.findById(id).map(DictTask::getInputType).orElse(null);
     }
 }
