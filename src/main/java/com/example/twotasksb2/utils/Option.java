623e37bfa0eeb8b7f3f10f11b7b6b134c5d2fffa
boyarov.a <boyarov.a@it-universe.ru>
2022-07-24 06:52:58 +0400

    Option (simple as possible)

---
+++ src/main/java/com/example/twotasksb2/utils/Option.java
@@ -0,0 +1,11 @@
+package com.example.twotasksb2.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Option {
+    private final Long value;
+    private final String label;
+}
