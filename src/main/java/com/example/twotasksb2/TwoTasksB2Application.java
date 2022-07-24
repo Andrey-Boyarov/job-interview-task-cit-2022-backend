30a30aea78f1c2dc01de444f3d3b1a3f588ae8bc
boyarov.a <boyarov.a@it-universe.ru>
2022-07-24 06:51:16 +0400

    Initial commit

---
+++ src/main/java/com/example/twotasksb2/TwoTasksB2Application.java
@@ -0,0 +1,13 @@
+package com.example.twotasksb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoTasksB2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoTasksB2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/twotasksb2/TwoTasksB2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twotasksb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoTasksB2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
